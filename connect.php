9241e2591d4e679dae8dd9ae6c161c1f9b6e5eef
Boonerd <126074329+Boonerd@users.noreply.github.com>
2023-07-19 13:17:01 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+	//connect to db server
+	$server = 'localhost';
+	$user = 'root';
+	$password = '';
+    $db = 'nothing';
+
+	$connect = mysqli_connect($server,$user,$password,$db);
+	if (!$connect){
+		echo "not successful";
+	}
+	
+    /*$sql = "CREATE DATABASE nothing";
+	$result = mysqli_query($connect,$sql);
+	if ($result) {
+		echo "db created successfully";
+	} else{
+		echo mysqli_error($connect);
+	}
+*/
+
+?>
